9f39735f62b3115d4f33577ae5f771157c3ede2c
abirafdi raditya putra <abirafdirp@yahoo.com>
2015-08-28 08:24:45 +0700

    delete idea folder

--- ppa/transaction/views.py
+++ ppa/transaction/views.py
@@ -1,7 +1,11 @@
 from .models import Transaction
 from django.shortcuts import render
+from django.utils import timezone
+from django.utils.timezone import now
+import datetime
 
 
 def display_today(request):
-    context = {'transactions': Transaction.objects.all()}
+    context = {'transactions':
+    Transaction.objects.all()}
     return render(request, template_name='today.html', context=context)
